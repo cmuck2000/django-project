9c043a634ea07b212a29f12408ebaba4e10e48f7
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-03-30 14:56:18 +0000

    add requirements.txt, migrate server to final db

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from environ import Env
-
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +73,18 @@ WSGI_APPLICATION = "django_todo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+DATABASES = {
+    "default": dj_database_url.parse(
+        "postgres://aweaxkegaxhuge:3e06ab1bcc4484051e63b2f97ed374cd018084614eea5952e5e2f74a71ad2e6d@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/dbv0cfj9dn39gg"
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
